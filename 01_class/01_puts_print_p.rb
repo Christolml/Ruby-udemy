5600380655aee98dc75e10cad7a078c7e3bf3800
christolml <christo_9722@hotmail.com>
2018-11-12 18:57:40 -0600

    métodos: puts, print y p

---
+++ 01_class/01_puts_print_p.rb
@@ -0,0 +1,57 @@
+
+
+# ------------------ puts -------------------
+
+puts "Hello world prros"
+puts "I'm live and well!"
+
+puts
+
+puts "Poniendo cosas"
+puts "En spanish y en inglés"
+
+puts 55
+puts 3.14159
+
+puts 4 + 2
+puts "4" + "2"
+
+puts
+puts
+puts
+
+puts "See, there's a bunch of line breaks above!"
+
+# ------------------ print -------------------
+#  comentario print hace lo mismo que puts solo que con print
+#  no me agrega un salto de linea al final y ningun tipo de espacio
+puts
+puts
+
+print "hola"
+print " "
+print "mundo que hubo"
+puts
+
+
+# ------------------ p -------------------
+# el método p nos hace lo mismo que puts pero la ventaja de usar p es que nos ofrece
+# una salida mas detalleda a lo que se refiere a la programación, por ejemplo nos indica en donde
+# hay un salto de linea
+puts
+puts
+
+puts "Christopher Velázquez"
+p "Christopher Velázquez"
+
+puts "se va a hacer la 
+carnita asada o no?"
+
+p "se va a hacer la
+carnita asada o no?"
+
+puts 5
+p 5
+
+p "hi there"
+p " otra cosa"
