9907f8b82b749abb1c2770f6bc218259167a92bb
christolml <christo_9722@hotmail.com>
2018-11-15 00:08:11 -0600

    aritmeticas

---
+++ 01_class/02_basic_arithmetic.rb
@@ -0,0 +1,31 @@
+# Operaciones aritméticas 
+p 1 + 4
+
+p 10 - 5
+
+p 3 * 4
+
+p "School" + "bus"
+
+# De esta forma ruby no pone los decimales de la división
+p 10 / 5
+p 12 / 5
+
+# De esta forma ya pone los decimales
+p 12.0 / 5
+p 12 / 5.0
+
+# Elevando a la potencia
+p 2 ** 5
+p 3 ** 3
+
+# Residuo
+p 5 % 2
+p 6 % 2
+
+p 16 % 14
+
+p 12 / 5 
+p 10 / 5
+
+p -0.75 / 2
