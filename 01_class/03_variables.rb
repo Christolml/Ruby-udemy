006400f7d67aa4b6b9520ed407bae6f393dcb1a2
christolml <christo_9722@hotmail.com>
2018-11-15 18:12:42 -0600

    variables, parallel variable and constants

---
+++ 01_class/03_variables.rb
@@ -0,0 +1,25 @@
+name = "Christopher"
+last_name = "Velázquez"
+handsome = true
+age = 18 + 4
+
+# a la hora de imprimir una contante con ayuda de + no  puedo juntar variables de diferente
+# tipo como por ejemplo puts last_name + age
+
+p last_name
+p handsome
+p age
+
+puts last_name
+puts name + " " + last_name 
+
+puts age +  8 
+
+# a una variable le puedo cambiar el tipo de dato, eso es tipado dinamico
+favorite_number = 20
+p favorite_number
+
+favorite_number = "veinte"
+p favorite_number
+
+
---
+++ 01_class/05_constants.rb
@@ -0,0 +1,23 @@
+age = 21
+p age
+
+age = 30 
+p age
+
+
+# Las contantes deben estar escritas en mayusculas
+
+
+NAME = "Christopher"
+PI = 3.1415
+
+p NAME
+p PI
+
+p "My name is #{NAME}"
+
+# lo de abajo si se ejecuta pero me marca un warning, ruby me indica que ya tenia un valor
+# predefinido y que por que le cambie
+NAME = "Emmanuelle"
+p NAME
+
