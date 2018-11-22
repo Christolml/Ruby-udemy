7d49ffec92f190d30f2e70d736378b15bba549cf
christolml <christo_9722@hotmail.com>
2018-11-21 22:18:44 -0600

    intro object method and string interpolation

---
+++ 01_class/06_intro_to_object_method.rb
@@ -0,0 +1,31 @@
+
+
+# devuelve la longitud de mi cadena
+p "hello world".length
+# convierte a mayusculas mi texto
+p "hello world".upcase
+# convierte a minisculas mi texto
+p "HELLO WORLD".downcase
+
+
+chris = "que hongo yo soy skra"
+p chris.length
+p chris.upcase
+
+
+puts
+# me da el siguiente valor de mi numero
+p 10.next
+p -3.next
+
+
+puts
+
+# inspect me imprime como si le pusiese con p
+puts "holi mundo in spanish prru"
+puts "holi mundo in spanish prru".inspect
+
+p "holi mundo in spanish prru"
+
+
+
---
+++ 01_class/07_string_interpolation.rb
@@ -0,0 +1,25 @@
+
+name = "Christopher"
+
+p "Holi #{name} como andas?"
+
+age = 21
+
+p "I am " + age.to_s + " years old."
+p "I am #{age} years old."
+
+p "The result of 1 + 1 is #{1 + 1}"
+
+p "In five years , I will be #{age + 5} years old."
+
+x = 5
+y = 8
+
+p "The result of x and y is #{x + y}"
+
+
+
+
+
+
+
